21d7d6490d514273a2513719630003bbfb207d5e
ZeshanRasul <zeshanrasul90@gmail.com>
2016-03-14 18:14:17 +0000

    Added test and code for player initialize

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,10 +1,10 @@
-# require 'player'
-#
-# describe 'Player' do
-#   let(player) {described_class.new}
-#
-#   it 'receives two cards' do
-#     expect(player.check_cards).to include(5,4)
-#   end
-#
-# end
+require 'player'
+
+describe Player do
+  let(:player) {described_class.new}
+
+  it 'starts with an empty hand' do
+    expect(subject.hand).to eq []
+  end
+
+end
